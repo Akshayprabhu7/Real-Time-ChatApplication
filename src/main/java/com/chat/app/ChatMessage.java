4efc4698d9dd2f8ecd1eb9ff96cdb345572b2193
N Akshay Prabhu <akshayprabhu2004@gmail.com>
2026-08-12 11:12:12 +0530

    Add WebSocket chat backend

---
+++ src/main/java/com/chat/app/ChatMessage.java
@@ -0,0 +1,20 @@
+package com.chat.app;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+
+    private String sender;
+    private String content;
+
+    public ChatMessage() {
+    }
+
+    public ChatMessage(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+    }
+}
